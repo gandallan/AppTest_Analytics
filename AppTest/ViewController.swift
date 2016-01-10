be888ef467bd037d328a34df88f4e61e8f3d7688
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2016-01-09 20:06:07 -0600

    nuevo commit

--- AppTest/ViewController.swift
+++ AppTest/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        let tracker = GAI.sharedInstance().defaultTracker
+        tracker.set(kGAIScreenName, value: "Tela Principal")
+        
+        let builder = GAIDictionaryBuilder.createScreenView()
+        tracker.send(builder.build() as [NSObject : AnyObject])
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
